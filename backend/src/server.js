46e7ec094f1f00b271dc4bf68d871a33182eee33
Jawed-lol <henijawed010@gmail.com>
2025-02-20 23:56:53 +0100

    Initialize Express backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { Pool } = require("pg");
 
 dotenv.config();

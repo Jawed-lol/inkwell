adbad555f877a13040869e166585a890852f95cc
Jawed-lol <henijawed010@gmail.com>
2025-02-20 23:38:35 +0100

    Initialize frontend

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,15 +1,14 @@
 export async function registerUser(name: string, surname: string, email: string, password: string) {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/register`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, surname, email, password }),
-    });
-  
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(errorData.error || 'Registration failed.');
-    }
-  
-    const data = await response.json();
-    return data.token; 
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/register`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, surname, email, password }),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.error || 'Registration failed.');
   }
+
+  return await response.json();
+}
